955831ec5ad855313201063a049fe84f60107216
Pheonix9001 <pheonix9001@gmail.com>
2024-04-10 22:31:36 +0530

    Add schema for question options

--- schema.sql
+++ schema.sql
@@ -1,5 +1,13 @@
+create type question_type as enum ('text', 'checkbox', 'radio');
 create table questions (
   id int primary key generated always as identity,
   ord int not null unique,
   description text
+);
+
+comment on table options is 'Only for checkbox and radio options';
+create table options (
+  question int references questions,
+  ord int not null unique,
+  primary key (question, ord)
 );
